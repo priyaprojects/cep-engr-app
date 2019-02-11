42ba89bfe7f49d2df3b9c6bc84b7c168177c92b3
ppandian <ppandian@homeaway.com>
2019-02-11 11:54:41 -0600

    Priya's Prework

--- src/init.js
+++ src/init.js
@@ -1,4 +1,41 @@
 // create a JavaScript object here with the following fields: firstName, lastName, jobTitle, homeOffice
+var candidate = new Object();
+var candidate = {
 
+	firstName: "Priya",
+	lastName: "Pandian",
+	jobTitle: "Test Engineer",
+  homeOffice: "Austin",
 
-// using jQuery and the object above, display the information as the appropriate button is clicked
+}
+
+// Here I grabbed each individual div and assigned information to each div from the candidate object
+document.getElementById("fname").innerHTML = candidate.firstName;
+document.getElementById("lname").innerHTML = candidate.lastName;
+document.getElementById("jtitle").innerHTML = candidate.jobTitle;
+document.getElementById("hoffice").innerHTML = candidate.homeOffice;
+
+//jQuery functions to display information according to the instructions below
+$(document).ready(function(){
+    $(".fname").click(function(){
+        $("#fname").slideDown("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".lname").click(function(){
+        $("#lname").slideDown("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".jtitle").click(function(){
+        $("#jtitle").slideDown("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".hoffice").click(function(){
+        $("#hoffice").slideDown("slow");
+    });
+});
